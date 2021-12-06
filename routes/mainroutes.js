0db05942d0b0d9a5a3addd08d06789199e0c0ec5
Pawankumar7814 <pawank4285@gmail.com>
2021-12-06 21:47:08 +0530

    made some changes

--- routes/mainroutes.js
+++ routes/mainroutes.js
@@ -2,15 +2,18 @@ let router = require('express').Router();
 
 //my routes
 router.get('/index', (req, res) => {
-    res.status(200).render('../views/index.ejs');
+    res.status(200).render('../views/mainpages/index.ejs');
 })
 
 router.get('/a', (req, res) => {
-    res.status(200).render('../views/About_us.ejs');
+    res.status(200).render('../views/mainpages/About_us.ejs');
 })
 
 router.get('/c', (req, res) => {
-    res.status(200).render('../views/Contact_us.ejs');
+    res.status(200).render('../views/mainpages/Contact_us.ejs');
 })
 
+router.get('/*', (req, res) => {
+    res.status(404).render('../views/mainpages/404.ejs');
+})
 module.exports = router;
